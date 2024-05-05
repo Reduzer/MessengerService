88d657083b28d7256c8467d827821210eded91a8
Reduzer <83287196+Reduzer@users.noreply.github.com>
2024-05-05 22:38:18 +0200

    05/05/2024 22:38
    Edit of Symetrical Encryption

--- Login/Security/encryptAsym.cs
+++ Login/Security/encryptAsym.cs
@@ -4,9 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+using System.Security.Cryptography;
+
 namespace Login.Security
 {
     internal class encryptAsym
     {
+
+
     }
 }
--- Login/Security/encryptSym.cs
+++ Login/Security/encryptSym.cs
@@ -4,9 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Security.Cryptography;
+
+
 namespace Login.Security
 {
     internal class encryptSym
     {
+        public encryptSym()
+        {
+
+        }
+
+        public string getEncryptetMessage(string sInput)
+        {
+            string sReturnString = "";
+
+            sReturnString = encryptSym(sInput);
+
+            return sReturnString;
+        }
+
+        private string encryptSym(string sInput)
+        {
+
+        }
+
+
     }
 }
