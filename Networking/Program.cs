0b4458055b55fa1b6b1dec5aef226ed6a19f7600
Reduzer <83287196+Reduzer@users.noreply.github.com>
2024-05-28 15:05:50 +0200

    Creating Base for interacting with the server
    Added the Classes for interacting with the server

--- Networking/Program.cs
+++ Networking/Program.cs
@@ -1,17 +1,40 @@
+using Networking.ConnectionTesting;
 using System;
+using System.Diagnostics;
 
 namespace networking
 {
     public class networking
     {
+        private static TestHandler tests = new TestHandler();
+
         public static void Main()
         {
-
+            if (testConnection())
+            {
+                Debug.WriteLine("Connection established");
+            }
+            else
+            {
+                Debug.WriteLine("Connection failed");
+            }
         }
 
         private static bool testConnection()  
         {
-            return false;
+            if (tests.testConnection())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void sendMessage()
+        {
+
         }
     }
 }
---
+++ Networking/connection/connectionHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Networking.connection
+{
+    internal class connectionHandler
+    {
+
+
+    }
+}
