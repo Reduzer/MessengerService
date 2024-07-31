d309c5b9465547eed6a8c88053053dd941942e77
Fyn <83287196+Reduzer@users.noreply.github.com>
2024-07-31 16:38:38 +0200

    JSON

---
+++ Login/JSON/JSONObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login.JSON
+{
+    internal class JSONObject
+    {
+        private string? sName { get; set; }
+        private string? sPassword { get; set; }
+        private string? sMac { get; set; }
+        private string? sUUID { get; set; }
+    }
+}
--- Login/JSON/ReadJSON.cs
+++ Login/JSON/ReadJSON.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Text.Json;
+
 namespace Login.JSON
 {
     internal class ReadJSON
